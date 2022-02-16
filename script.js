9165cdbda82fa20a238871edb1940c9d275b8838
jvborges25 <jvborges25@gmail.com>
2022-02-16 13:40:09 -0300

    primeira atualização

---
+++ script.js
@@ -0,0 +1,28 @@
+function contar() {
+   let ini = document.getElementById('txti')
+   let fim = document.getElementById('txtf')
+   let passo = document.getElementById('txtp')
+   let res = document.getElementById('res')
+
+   if(ini.Value == 0 || fim.Value == 0 || passo.Value == 0) {
+       window.alert('[ERRO] Faltam dados!')  
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            // contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} `
+            }
+        } else {
+            // contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} `
+            } 
+        }
+        res.innerHTML += `${c}`
+    }
+}
+
